70c9c8e9daa400d622e600e81dff7e4a65279add
jafarpour <jafarpour@caspian.com>
2026-01-07 15:35:42 +0330

    singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var obj1 = SingletonDE.GetInstance();
+        var obj2 = SingletonDE.GetInstance();   
+        Console.WriteLine( obj1 == obj2 );  
+        Console.ReadKey();
+    }
+}
---
+++ DesignPatterns/Singleton/SingletonDE.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton
+{
+    public sealed class SingletonDE
+    {
+        private readonly static Lazy<SingletonDE> uniqSingletonDE = new Lazy<SingletonDE>();
+        private SingletonDE()
+        {
+
+        }
+
+        public static SingletonDE GetInstance()
+        {
+            return uniqSingletonDE.Value;
+        }
+    }
+}
